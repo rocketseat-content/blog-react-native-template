e752208c8bff3f5f58b0e41a15d45df6b74e2524
Diego Fernandes <diego.schell.f@gmail.com>
2018-05-07 14:52:04 -0300

    Template

---
+++ src/pages/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+const Main = () => (
+  <View>
+    <Text>Main page</Text>
+  </View>
+);
+
+export default Main;
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import { StackNavigator } from 'react-navigation';
+
+import Main from './pages/main';
+
+export default StackNavigator({
+  Main: { screen: Main },
+});
